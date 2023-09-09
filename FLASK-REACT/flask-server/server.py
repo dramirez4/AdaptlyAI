b3d314ff9ba1597617dcd536f35cca820d368e92
Jason Antwi-Appah <hey@jasonaa.me>
2023-09-09 10:11:52 -0400

    Enable CORS on API

--- FLASK-REACT/flask-server/server.py
+++ FLASK-REACT/flask-server/server.py
@@ -1,8 +1,10 @@
 # Import the Flask class from the Flask library
 from flask import Flask 
+from flask_cors import CORS
 
 # Create an instance of the Flask class and name it 'app'
 app = Flask(__name__)
+CORS(app)
 
 # Define a route for the '/members' endpoint
 @app.route("/members")
