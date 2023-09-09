3d25713d0175b45f80ca9d6dfd9347e2f28e682e
supersillyswiper <dramirez2@binghamton.edu>
2023-09-09 01:08:15 -0400

    made flask-server and react frontend(client folder) and connected to display data

---
+++ FLASK-REACT/flask-server/server.py
@@ -0,0 +1,17 @@
+# Import the Flask class from the Flask library
+from flask import Flask 
+
+# Create an instance of the Flask class and name it 'app'
+app = Flask(__name__)
+
+# Define a route for the '/members' endpoint
+@app.route("/members")
+def members():
+    # Return a JSON response with a list of members
+    return {"members": ["Member1", "Member2", "Member3"]}
+
+# This condition ensures that the app is only run if this script is the main program
+if __name__ == "__main__":
+    # Run the app in debug mode, which provides detailed error messages
+    app.run(debug=True)
+
