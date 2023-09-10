c4527111e378e29e358e11f89044d765fb53ded9
ElijahUmana <elijah@uni.minerva.edu>
2023-09-09 21:51:50 -0400

    commit

--- FLASK-REACT/flask-server/views.py
+++ FLASK-REACT/flask-server/views.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+# This file will contain our Flask route handlers (views) related to database operations. 
+from flask import request, jsonify
+from app import app
+from models import User
 
-bp = Blueprint('views', __name__)
-
-@bp.route('/')
-def home():
-    return "Welcome to the Learning App!"
+@app.route('/onboard', methods=['POST'])
+def onboard_user():
+    data = request.get_json()
+    user_id = User.create_user(data)
+    return jsonify({"message": "User onboarded successfully", "user_id": str(user_id.inserted_id)}), 201
