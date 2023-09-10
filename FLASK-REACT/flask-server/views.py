06f1e05fcef94bed380096e60091f17dd8740f98
ElijahUmana <elijah@uni.minerva.edu>
2023-09-10 02:06:47 -0400

    Implement user onboard speech to text

--- FLASK-REACT/flask-server/views.py
+++ FLASK-REACT/flask-server/views.py
@@ -1,10 +1,11 @@
-# This file will contain our Flask route handlers (views) related to database operations. 
-from flask import request, jsonify
-from app import app
-from models import User
 
-@app.route('/onboard', methods=['POST'])
+# This file will contain our Flask route handlers (views) related to database operations. 
+from flask import Blueprint, request, jsonify
+
+bp = Blueprint('views', __name__)
+@bp.route('/onboard', methods=['POST'])
 def onboard_user():
+    from models import User
     data = request.get_json()
     user_id = User.create_user(data)
     return jsonify({"message": "User onboarded successfully", "user_id": str(user_id.inserted_id)}), 201
