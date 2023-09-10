49a5020fd2daa55d3536fc7dacb4bab3ee072500
Jason Antwi-Appah <hey@jasonaa.me>
2023-09-10 05:51:03 -0400

    Create deck support on BE

--- FLASK-REACT/flask-server/gpt4_api.py
+++ FLASK-REACT/flask-server/gpt4_api.py
@@ -1,5 +1,7 @@
 import requests
 import config
+import openai
+# TODO: add to reqs.txt
 
 def call_gpt4_to_extract_info(transcript):
     ENDPOINT = "https://api.openai.com/v1/chat/completions"
@@ -50,3 +52,61 @@ def call_gpt4_to_extract_info(transcript):
         "style": style_summary,
         "language": language_summary
     }
+
+
+def PROMPT_BASE_TEMPLATE(student_info: str):
+    return f'You are an expert educator. Help your student learn new materials by creating engaging and accessible slideshows that cater to their learning style and background. Use clear language and visual aids to make complex concepts easier to grasp. Use analogies and other learning devices that best fit their needs based on the following information about the student: """{student_info}"""'
+
+def NEW_DECK_PROMPT_TEMPLATE(query: str):
+    return f'I will be learning about """{query}""". Return a newline-delimited-list of slide titles that you would include in a slideshow used to teach me about """{query}""". Do not generate the content associated with the slides until asked.'
+
+def CONFUSED_PROMPT_TEMPLATE(confused_slide_title: str):
+    return f'I was confused by this slide: """{confused_slide_title}""". Return an amended new-line-delimited list of slide titles, and include at least 1, but no more than 3, new slides that you would add to the slide deck, breaking down the given slide\'s topic into simpler parts until it\'s at just the right level for me to gain an understanding and help me resolve my doubts.'
+
+def GEN_SLIDE_PROMPT_TEMPLATE(query: str, slide_title: str):
+    return f'"I\'m learning about """{query}""", currently on the slide """{slide_title}""". Create the content of this slide. Utilize Markdown for rich text formatting. Do not generate any images or image URLs. Do not be overly verbose. Do not unnecessarily repeat content that will be covered on other slides within the deck. Be sure to keep previously given information about me in mind.'
+
+def make_new_deck_prompt(student_info: str, query: str):
+    return [{"role": "system", "content": PROMPT_BASE_TEMPLATE(student_info)},
+    {"role": "user", "content": NEW_DECK_PROMPT_TEMPLATE(query)}]
+
+def make_new_deck(student_info: str, query: str):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=make_new_deck_prompt(student_info, query)
+        
+    )
+    print(response)
+    return deck_res_to_list(response)
+
+def deck_res_to_list(res):
+    return res_str(res).split('\n')
+
+def res_str(res):
+    return res['choices'][0]['message']['content']
+
+def make_new_slide_prompt(student_info: str, query: str, slide_title: str, slide_titles: list[str]):
+    return [{"role": "system", "content": PROMPT_BASE_TEMPLATE(student_info)},
+    {"role": "assistant", "content": "\n".join(slide_titles)},
+    {"role": "user", "content": GEN_SLIDE_PROMPT_TEMPLATE(query, slide_title)}]
+
+def make_new_slide(student_info: str, query: str, slide_title: str, slide_titles: list[str]):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=make_new_slide_prompt(student_info, query, slide_title, slide_titles)
+        
+    )
+    print(response)
+    return res_str(response)
+
+def make_confused_deck(student_info: str, query: str, confused_slide_title: str, slide_titles: list[str]):
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            *make_new_deck_prompt(student_info, query),
+            {"role": "assistant", "content": "\n".join(slide_titles)},
+            {"role": "user", "content": CONFUSED_PROMPT_TEMPLATE(confused_slide_title)},
+        ]
+    )
+    print(response)
+    return deck_res_to_list(response)
